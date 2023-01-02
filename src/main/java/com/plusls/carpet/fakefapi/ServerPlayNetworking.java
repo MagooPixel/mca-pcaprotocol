397d8104e2b9bb837c45b96cd6ec0609dbe25626
麻瓜瓜 <master@magicst.cn>
2023-01-02 18:55:43 +0800

    Change : update to 1.19.3

--- src/main/java/com/plusls/carpet/fakefapi/ServerPlayNetworking.java
+++ src/main/java/com/plusls/carpet/fakefapi/ServerPlayNetworking.java
@@ -4,10 +4,11 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 
 public class ServerPlayNetworking
 {
-    public static void send(ServerPlayerEntity player, Identifier identifier, PacketByteBuf buf)
+    public static void send(@NotNull ServerPlayerEntity player, Identifier identifier, PacketByteBuf buf)
     {
         player.networkHandler.sendPacket(new CustomPayloadS2CPacket(identifier, buf));
     }
